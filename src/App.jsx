4cf2658cdd461b297b3831fe2fc90d16f16d5881
Dan Russo <rdaniele669@gmail.com>
2025-05-16 11:29:48 +0200

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Welcome from "./components/Welcome";
 // import AllTheBooks from "./components/AllTheBooks";
 import BookList from "./components/BookList";
 import fantasy from "./array/fantasy.json";
-import horror from "./array/horror.json";
-import romance from "./array/romance.json";
-import scifi from "./array/scifi.json";
-import history from "./array/history.json";
+// import horror from "./array/horror.json";
+// import romance from "./array/romance.json";
+// import scifi from "./array/scifi.json";
+// import history from "./array/history.json";
 
 function App() {
   return (
